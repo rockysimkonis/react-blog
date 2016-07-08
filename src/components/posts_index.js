47a6ba6d452b12f60f01428ddee6c64878359aee
Rocky Simkonis <rokas.simkonis@gmail.com>
2016-07-08 17:30:11 -0500

    Adding button that routes and links to a new post

--- src/components/posts_index.js
+++ src/components/posts_index.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { bindActionCreators } from 'redux';
+// import { bindActionCreators } from 'redux';
 import { fetchPosts } from '../actions/index';
+import { Link } from 'react-router'
 
 class PostsIndex extends Component {
   componentWillMount() {
@@ -10,7 +11,14 @@ class PostsIndex extends Component {
 
   render() {
     return (
-      <div>List of all blog posts.</div>
+      <div>
+        <div className="text-xs-right">
+          <Link to="/posts/new" className="btn btn-primary">
+            Add a Post
+          </Link>
+        </div>
+        List of all blog posts.
+      </div>
     );
   }
 }
